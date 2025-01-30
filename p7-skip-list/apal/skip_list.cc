d38f1c1fb14c1ffbeff9a53620e4037927f3efab
Austin Maddison <Austin.madison10152@gmail.com>
2025-01-30 13:45:26 +0700

    fix: insertion works and print works

--- p7-skip-list/apal/skip_list.cc
+++ p7-skip-list/apal/skip_list.cc
@@ -27,12 +27,11 @@ namespace apal {
 
   void SkipList::init_header() {
     header = std::vector<Node*>(LEVEL_HEIGHT_MAX);
-    auto curr = new Node(INT_MAX, 0, nullptr, nullptr, nullptr);
+    auto curr = new Node(-INT_MAX, 0, nullptr, nullptr, nullptr);
     header[curr->level] = curr;
 
-
     while (curr->level < LEVEL_HEIGHT_MAX-1) {
-      curr->up = new Node(INT_MAX, curr->level + 1, nullptr, nullptr, curr);
+      curr->up = new Node(-INT_MAX, curr->level + 1, nullptr, nullptr, curr);
       curr = curr->up;
       header[curr->level] = curr;
     }
@@ -41,50 +40,64 @@ namespace apal {
   }
 
 
-  void SkipList::insert(int key) {
+  void SkipList::insert(const int key) {
 
     const u_int64 tosses = distribution(generator);
     u_int64 levels = std::countr_one(tosses); // coin tosses
 
     // search
-    Node* curr = head;
+    Node* cursor_curr = head;
+    Node* cursor_prev = cursor_curr;
 
     while (true) {
 
+      bool is_end = (cursor_curr->down == nullptr && cursor_curr->next == nullptr);
+
       // insertion point found
-      if (!curr->key <= key || (curr->down == nullptr && curr->next == nullptr)) {
+      if (cursor_curr->key >= key || is_end) {
+        cursor_curr = cursor_prev;
         break;
       }
 
       // end of level
-      if (curr->next == nullptr) {
-        curr = curr->down;
+      if (cursor_curr->next == nullptr) {
+        cursor_curr = cursor_curr->down;
         continue;
       }
 
-      curr = curr->next->key > key ? curr->down : curr->next;
-    }
-    // go to level 0
-    while (curr->down != nullptr) {
-      curr = curr->down;
-    }
-
-    curr->next = new Node(key, 0, curr->next, nullptr, nullptr);
-    Node* inserted_prev = curr->next;
-
-    // promote key
-    for (int i = 1; i < levels; i++) {
-
-      curr = header[i];
-
-      while (curr->next != nullptr) {
-        if (curr->key <= key) {
-          break;
-        }
+      if (cursor_curr->next->key > key) {
+        cursor_curr = cursor_curr->down;
+      }
+      else {
+        cursor_curr = cursor_curr->next;
       }
 
-      curr->next = new Node(key, i, curr->next, nullptr, inserted_prev);
-      inserted_prev = curr->next;
+      cursor_prev = cursor_curr;
+    }
+
+    // go to level 0
+    while (cursor_curr->down != nullptr) {
+      cursor_curr = cursor_curr->down;
+    }
+
+    // insert, from head, promote inserted node from level 0
+    cursor_curr->next = new Node(key, 0, cursor_curr->next, nullptr, nullptr);
+    Node* inserted_node = cursor_curr->next;
+
+    for (int i = 1; i < levels; i++) {
+      cursor_curr = header[i];
+
+      while (cursor_curr->next != nullptr) {
+        if (cursor_curr->key >= key) {
+          cursor_curr = cursor_prev;
+          break;
+        }
+        cursor_curr = cursor_curr->next;
+      }
+
+      cursor_curr->next = new Node(key, i, cursor_curr->next, nullptr, inserted_node);
+      inserted_node->up = cursor_curr->next;
+      inserted_node = cursor_curr->next;
     }
 
     size++;
@@ -92,8 +105,11 @@ namespace apal {
 
 
   void SkipList::remove(int key) {}
+
   int SkipList::search(int key) const { return -1; }
+
   int SkipList::get_size() const { return this->size; }
+
   void SkipList::print() const {
     if (header[0]->next == nullptr) {
       return;
@@ -102,26 +118,27 @@ namespace apal {
     std::vector<std::tuple<int, int>> data;
 
     int max_level = 0;
-    auto curr = header[0]->next;
+    auto level_0_curr = header[0]->next;
+
     for (int i = 0; i < size; i++) {
+      auto curr = level_0_curr;
 
       while (curr->up != nullptr) {
         curr = curr->up;
       }
-
       data.push_back(std::make_tuple(curr->key, curr->level));
       max_level = std::max(max_level, curr->level);
-      curr = curr->next;
+      level_0_curr = level_0_curr->next;
     }
 
     for (int i = max_level; i >= 0; i--) {
-      std::cout << i << " | ";
+      std::cout << 'h' << i << " | ";
       for (int j = 0; j < size; j++) {
         if (std::get<1>(data[j]) - i >= 0) {
           std::cout << std::get<0>(data[j]) << "  ";
         }
         else {
-          std::cout << std::get<0>(data[j]) << "   ";
+          std::cout << "   ";
         }
       }
       std::cout << '\n';
