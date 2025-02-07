0e7e2ba6a83505a0bb944d3b548cbd10f803bd9f
Austin Maddison <Austin.madison10152@gmail.com>
2025-02-08 02:28:59 +0700

    Toss coin benches

---
+++ notebook/my_settings.py
@@ -0,0 +1,11 @@
+# Libraries and settings for the homework 1
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+from matplotlib import cm
+import seaborn as sns
+
+sns.set_theme(font_scale=1.5, rc={'text.usetex' : True})
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = r'\usepackage{lmodern}'
