09601fad4bd47a8bb3ebc069462a0963dd61ac03
Austin Maddison <Austin.madison10152@gmail.com>
2025-02-10 05:12:31 +0700

    .

---
+++ code/src/becnh_over_ml.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <psapi.h>
+#include <benchmark/benchmark.h>
+#include <set>
+#include <numeric>
+#include <random>
+#include "skip_list.h"
+
+inline static size_t GetMemRam() {
+  PROCESS_MEMORY_COUNTERS memInfo;
+  if (GetProcessMemoryInfo(GetCurrentProcess(), &memInfo, sizeof(memInfo))) {
+    return memInfo.WorkingSetSize / 1024; // KB
+  }
+  return -1;
+}
+
+inline static size_t GetMemPagefile() {
+  PROCESS_MEMORY_COUNTERS memInfo;
+  if (GetProcessMemoryInfo(GetCurrentProcess(), &memInfo, sizeof(memInfo))) {
+    return memInfo.PagefileUsage / 1024; // KB
+  }
+  return -1;
+}
+
+inline static int GetRandomInt(int max) {
+  std::mt19937 rng(std::random_device{}());
+  std::uniform_int_distribution<int> dist(0, max);
+  return dist(rng);
+}
+
+inline static std::vector<int> GetRandomVector(int n) {
+  std::vector<int> keys(n);
+  std::iota(keys.begin(), keys.end(), 0);
+  std::shuffle(keys.begin(), keys.end(), std::mt19937{ std::random_device{}() });
+  return keys;
+}
+
+template <int L>
+static void SkipList_Insertion(benchmark::State& state) {
+  int n = state.range(0);
+
+  std::vector<int> keys = GetRandomVector(n);
+  double memRam = GetMemRam(), memPage = GetMemPagefile();
+  auto sl = apal::SkipList<L>();
+
+  for (auto _ : state) {
+    for (auto key : keys) { benchmark::DoNotOptimize(sl.insert(key)); }
+  }
+
+  state.counters["RAM"] = double(GetMemRam()) - memRam;
+  state.counters["Page"] = double(GetMemPagefile()) - memPage;
+  state.SetItemsProcessed(state.iterations());
+
+}
+
+static void OrderedMap_Insertion(benchmark::State& state) {
+  int n = state.range(0);
+
+  std::vector<int> keys = GetRandomVector(n);
+  double memRam = GetMemRam(), memPage = GetMemPagefile();
+  std::set<int> ordered_set;
+
+  for (auto _ : state) {
+    state.PauseTiming();
+    for (auto key : keys) { benchmark::DoNotOptimize(ordered_set.insert(key)); }
+  }
+
+  state.counters["RAM"] = double(GetMemRam()) - memRam;
+  state.counters["Page"] = double(GetMemPagefile()) - memPage;
+  state.SetItemsProcessed(state.iterations());
+}
+
+template <int L>
+static void SkipList_Search(benchmark::State& state) {
+  int n = state.range(0);
+
+  std::vector<int> keys = GetRandomVector(n);
+  double memRam = GetMemRam(), memPage = GetMemPagefile();
+  auto sl = apal::SkipList<L>();
+  for (auto key : keys) { sl.insert(key); }
+
+  for (auto _ : state) {
+    for (auto key : keys) { benchmark::DoNotOptimize(sl.contains(key)); }
+  }
+
+  state.counters["RAM"] = double(GetMemRam()) - memRam;
+  state.counters["Page"] = double(GetMemPagefile()) - memPage;
+  state.SetItemsProcessed(state.iterations());
+}
+
+static void OrderedMap_Search(benchmark::State& state) {
+  int n = state.range(0);
+
+  std::vector<int> keys = GetRandomVector(n);
+  double memRam = GetMemRam(), memPage = GetMemPagefile();
+  std::set<int> ordered_set;
+  for (auto key : keys) { ordered_set.insert(key); }
+
+  for (auto _ : state) {
+    for (auto key : keys) { benchmark::DoNotOptimize(ordered_set.contains(key)); }
+  }
+
+  state.counters["RAM"] = double(GetMemRam()) - memRam;
+  state.counters["Page"] = double(GetMemPagefile()) - memPage;
+  state.SetItemsProcessed(state.iterations());
+}
+
+// New
+
+//BENCHMARK(OrderedMap_Insertion)->RangeMultiplier(2)->Range(8, 8 << 8)->MinWarmUpTime(1)->Iterations(8 << 14);
+//BENCHMARK(OrderedMap_Insertion)->RangeMultiplier(2)->Range(8 << 9, 8 << 16)->MinWarmUpTime(1)->Iterations(8 << 16);
+//BENCHMARK(OrderedMap_Insertion)->RangeMultiplier(2)->Range(8 << 16, 8 << 20)->MinWarmUpTime(0)->Iterations(8);
+//
+//BENCHMARK(SkipList_Insertion)->RangeMultiplier(2)->Range(8, 8 << 8)->MinWarmUpTime(1)->Iterations(8 << 14);
+//BENCHMARK(SkipList_Insertion)->RangeMultiplier(2)->Range(8 << 9, 8 << 16)->MinWarmUpTime(1)->Iterations(8 << 16);
+//BENCHMARK(SkipList_Insertion)->RangeMultiplier(2)->Range(8 << 16, 8 << 20)->MinWarmUpTime(0)->Iterations(8);
+//
+//BENCHMARK(OrderedMap_Search)->RangeMultiplier(2)->Range(8, 8 << 8)->MinWarmUpTime(1)->Iterations(8 << 14);
+//BENCHMARK(OrderedMap_Search)->RangeMultiplier(2)->Range(8 << 9, 8 << 16)->MinWarmUpTime(1)->Iterations(8 << 16);
+//BENCHMARK(OrderedMap_Search)->RangeMultiplier(2)->Range(8 << 16, 8 << 20)->MinWarmUpTime(0)->Iterations(8);
+//
+//BENCHMARK(SkipList_Search)->RangeMultiplier(2)->Range(8, 8 << 8)->MinWarmUpTime(1)->Iterations(8 << 14);
+//BENCHMARK(SkipList_Search)->RangeMultiplier(2)->Range(8 << 9, 8 << 16)->MinWarmUpTime(1)->Iterations(8 << 16);
+//BENCHMARK(SkipList_Search)->RangeMultiplier(2)->Range(8 << 16, 8 << 20)->MinWarmUpTime(0)->Iterations(8);
+
+// Preliminary
+
+
+BENCHMARK_TEMPLATE(SkipList_Insertion, 2)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 4)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 6)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 8)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 10)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 12)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 14)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 16)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 18)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 20)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 22)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 24)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 26)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 28)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 30)->Arg(8<<14);
+BENCHMARK_TEMPLATE(SkipList_Insertion, 32)->Arg(8<<14);
+
+BENCHMARK_TEMPLATE(SkipList_Search, 2)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 4)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 6)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 8)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 10)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 12)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 14)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 16)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 18)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 20)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 22)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 24)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 26)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 28)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 30)->Arg(8 << 14);
+BENCHMARK_TEMPLATE(SkipList_Search, 32)->Arg(8 << 14);
+
+BENCHMARK_MAIN();
--- code/src/skip_list.h
+++ code/src/skip_list.h
@@ -3,12 +3,24 @@
 #include <random>
 #include <iterator>
 #include <type_traits>
+#include <bits.h>
+#include <stdint.h>
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <tuple>
+#include <cassert>
+
 
 #include "skip_list_node.h"
 
+#define DEFAULT_MAX_LEVEL 32
 
 namespace apal {
 
+  template<int ML = DEFAULT_MAX_LEVEL>
   class SkipList {
   public:
 
@@ -19,7 +31,6 @@ namespace apal {
     bool insert(uint64_t key);
     bool remove(uint64_t key);
     bool contains(uint64_t key) const;
-    void clear();
     [[nodiscard]] size_t size() const;
 
     void print_full() const;
@@ -51,6 +62,182 @@ namespace apal {
 
     inline unsigned int random_level();
   };
+
+
+  template<int ML>
+  SkipList<ML>::SkipList()
+    : m_head(nullptr),
+    m_level(0) {
+
+    m_distribution = std::uniform_int_distribution<uint32_t>(0, UINT32_MAX);
+#ifdef DEBUG_STATIC_SEED
+    m_generator.seed(DEBUG_STATIC_SEED);
+#else
+    m_generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
+#endif
+
+    m_head = create_node(ML, uint64_t());
+  }
+
+  template<int ML>
+  SkipList<ML>::~SkipList() {
+    while (m_head) {
+      SkipListNode* to_destroy = m_head;
+      m_head = m_head->next[0];
+      destroy_node(to_destroy);
+    }
+  }
+
+  template<int ML>
+  bool SkipList<ML>::contains(uint64_t key) const {
+
+    const SkipListNode* node = m_head;
+    for (int i = level; i >= 0; --i) {
+      while (node->next[i] && node->next[i]->key < key)
+        node = node->next[i];
+    }
+
+    node = node->next[0];
+    return node && node->key == key;
+  }
+
+  template<int ML>
+  bool SkipList<ML>::insert(uint64_t key) {
+
+    auto node = m_head;
+    SkipListNode* update[ML + 1];
+    memset(update, 0, sizeof(SkipListNode*) * (ML + 1));
+
+    // search and store references for new node.
+    for (int i = level; i >= 0; i--) {
+      while (node->next[i] && node->next[i]->key < key) {
+        node = node->next[i];
+      }
+      update[i] = node;
+    }
+    node = node->next[0];
+
+    // check unique
+    if (node != nullptr && node->key == key) {
+      return false;
+    }
+
+    auto new_level = random_level();
+
+    // new levels, no neighbors to possibly skip to, point to head
+    if (new_level > level) {
+      for (int i = level + 1; i <= new_level; i++) {
+        update[i] = m_head;
+      }
+      level = new_level;
+    }
+    node = create_node(new_level, key);
+
+    // stitch the new node into the structure
+    for (int i = 0; i <= new_level; i++) {
+      node->next[i] = update[i]->next[i];
+      update[i]->next[i] = node;
+    }
+
+    m_size++;
+    return true;
+  }
+
+  template<int ML>
+  bool SkipList<ML>::remove(uint64_t key) {
+    SkipListNode* node = m_head;
+    SkipListNode* update[ML + 1];
+    memset(update, 0, sizeof(SkipListNode*) * (ML + 1));
+
+    for (int i = level; i >= 0; i--)
+    {
+      while (node->next[i] && node->next[i]->key < key)
+        node = node->next[i];
+      update[i] = node;
+    }
+    node = node->next[0];
+
+    if (node->key == key)
+    {
+      for (int i = 0; i <= level; i++) {
+        if (update[i]->next[i] != node)
+          break;
+        update[i]->next[i] = node->next[i];
+      }
+      destroy_node(node);
+      while (level > 0 && !m_head->next[level])
+        --level;
+      return true;
+    }
+    return false;
+  }
+
+  template<int ML>
+  size_t SkipList<ML>::size() const {
+    return m_size;
+  }
+
+  template<int ML>
+  void SkipList<ML>::print_full() const {
+    /* if (m_size == 0) {
+       return;
+     }
+
+     std::vector<std::tuple<int, size_t>> key_level;
+     auto node_curr = m_head->next[0];
+
+     for (auto node_curr = m_head->next[0]; node_curr != m_null; ) {
+       key_level.emplace_back(node_curr->key, node_curr->next.size()-1);
+       node_curr = node_curr->next[0];
+     }
+
+     int padding = 6;
+
+     for (int i = m_level; i >= 0; i--) {
+       std::cout << std::setw(padding) << 'h';
+       std::cout << " | ";
+
+       for (int j = 0; j < m_size; j++) {
+
+         auto key = std::get<0>(key_level[j]);
+         auto level = std::get<1>(key_level[j]);
+
+         if (i <= level) {
+           std::cout << std::setw(padding) << key;
+         }
+         else {
+           std::cout << std::setw(padding) << ' ';
+         }
+       }
+       std::cout << '\n';
+     }*/
+  }
+
+  template<int ML>
+  apal::SkipListNode* SkipList<ML>::create_node(int level, const uint64_t new_key) {
+    void* node_mem = malloc(sizeof(SkipListNode));
+    SkipListNode* new_node = static_cast<SkipListNode*>(node_mem);
+    new (&new_node->key) uint64_t(new_key);
+
+    void* next_mem = calloc(level + 1, sizeof(SkipListNode*));
+    new_node->next = static_cast<SkipListNode**>(next_mem);
+    return new_node;
+  }
+
+  template<int ML>
+  void SkipList<ML>::destroy_node(SkipListNode* node)
+  {
+    free(node->next);
+    free(node);
+  }
+
+  template<int ML>
+  inline unsigned int SkipList<ML>::random_level() {
+    auto rand = m_distribution(m_generator);
+
+    return __lzcnt(rand) % ML;
+  }
+
 
 } // apal
 
